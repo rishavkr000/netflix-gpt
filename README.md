11b9d32f884d46eecb7cdf3ecd53560cbce2c79b
Rishav Kumar <rishavkr000@gmail.com>
2025-05-31 08:13:16 +0530

    Create vite + React app and initialized Tailwind CSS

---
+++ README.md
@@ -0,0 +1,6 @@
+# Netflix GPT
+
+- Create react app using vite
+    - npm create vite@latest netfilx-gpt -- --template react
+- Configured Tailwind CSS
+    - 
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="m-4 text-3xl font-bold text-red-300">Hello Everyone!</div>;
+}
+
+export default App;
