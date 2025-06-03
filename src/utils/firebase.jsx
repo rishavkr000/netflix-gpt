1f046be620b42626c0c426e9ef97105e883d78c6
Rishav Kumar <rishavkr000@gmail.com>
2025-06-03 16:27:36 +0530

    Keep firebase secret in .env file

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoFpNAdEBHERmaI422e-rVfFMZ_04WCj4",
-  authDomain: "netflixgpt-b497e.firebaseapp.com",
-  projectId: "netflixgpt-b497e",
-  storageBucket: "netflixgpt-b497e.firebasestorage.app",
-  messagingSenderId: "1028598283464",
-  appId: "1:1028598283464:web:d7dd66c27280c3be8439fd",
-  measurementId: "G-W82HD9PJH1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
